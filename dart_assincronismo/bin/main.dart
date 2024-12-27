0bedeb67dcf7effbddc36a97920d68627363f7df
karlinhos987 <karlinhos987@hotmail.com>
2024-12-26 21:15:27 -0300

    exemplo

--- dart_assincronismo/bin/main.dart
+++ dart_assincronismo/bin/main.dart
@@ -1,9 +1,17 @@
 import 'package:http/http.dart';
 import 'dart:convert';
 
+import 'package:main/api_key.dart';
+
 void main() {
   //requestData();
-  requestDataAsync();
+  //requestDataAsync();
+  sendDataAsync({
+    "id": "NEW001",
+    "name": "Futter",
+    "lastname": "Dart",
+    "balance": 10000,
+  });
 }
 
 requestData() {
@@ -28,12 +36,37 @@ requestData() {
   print("End of requestData");
 }
 
-requestDataAsync() async {
+Future<List<dynamic>> requestDataAsync() async {
   String url =
       "https://gist.githubusercontent.com/karlinhos987/3b2cb971f0bd6cbb37ad69bb03d935de/raw/461bc24ce58ec01522c7d3235934fd87a0d80a7e/json";
   Response response = await get(Uri.parse(url));
 
-  print(json.decode(response.body)[0]);
+  //print(json.decode(response.body)[0]);
+  //print("End of requestDataAsync");
+  return json.decode(response.body);
+}
 
-  print("End of requestDataAsync");
+sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+  //print(content);
+
+  String url = "https://api.github.com/gists/3b2cb971f0bd6cbb37ad69bb03d935de";
+
+  Response response = await post(
+    Uri.parse(url),
+    headers: {"Authorization": "Bearer $gitHubApiKey"},
+    body: json.encode({
+      "description": "json",
+      "public": true,
+      "files": {
+        "json": {
+          "content": content,
+        },
+      },
+    }),
+  );
+
+  print(response.statusCode);
 }
